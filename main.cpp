7e41805f23bb23e717d3b46994cb3c6eefb97d58
Michael Aylesbury <moaylesbury@gmail.com>
2022-04-01 14:07:32 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,96 +5,161 @@
 
 #include <infos.h>
 
-const char* concat_char_arr(const char* lhs, const char* rhs){
-    // concatenates two strings
+// HDIR get_concat_dir(const char* lhs, const char* rhs){
+//     // concatenates two strings
     
-    int lhs_size = strlen(lhs);
-    int rhs_size = strlen(rhs);
+//     int lhs_size = strlen(lhs);
+//     int rhs_size = strlen(rhs);
 
-    // printf("this word %s has size %d \n", lhs, lhs_size);
-    // printf("this word %s has size %d \n", rhs, rhs_size);
+//     // printf("this word %s has size %d \n", lhs, lhs_size);
+//     // printf("this word %s has size %d \n", rhs, rhs_size);
 
-    int new_size = lhs_size + rhs_size;
+//     int new_size = lhs_size + rhs_size;
 
-    char concat[new_size+1];
-    concat[new_size+1] = 0;
-    // char* concat = new char[new_size];
-    // _data = new char[_size + 1];
+//     char concat[new_size+1];
+//     concat[new_size+1] = 0;
+//     // char* concat = new char[new_size];
+//     // _data = new char[_size + 1];
     
 
-    // printf("the new size is %d \n", new_size);
+//     // printf("the new size is %d \n", new_size);
 
-    // need to add the slash at the relevant location
+//     // need to add the slash at the relevant location
     
    
 
-    for (unsigned int i=0; i < lhs_size; i++) {
-        // printf("%d\n", i);
-        concat[i] = lhs[i];
-    }
-    concat[lhs_size] = *"/";
+//     for (unsigned int i=0; i < lhs_size; i++) {
+//         // printf("%d\n", i);
+//         concat[i] = lhs[i];
+//     }
+//     concat[lhs_size] = *"/";
 
-    printf("\n");
+//     printf("\n");
 
-    for (unsigned int i=0; i < rhs_size; i++) { 
-        // printf("%d\n", lhs_size+i);
-        concat[lhs_size+i+1] = rhs[i];
-    }
-
-
-    // printf("\n");
-    const char* i = concat;
-    printf("concatenation: %s\n", concat);
-    return i;
-}
+//     for (unsigned int i=0; i < rhs_size; i++) { 
+//         // printf("%d\n", lhs_size+i);
+//         concat[lhs_size+i+1] = rhs[i];
+//     }
 
 
+//     // printf("\n");
+//     // const char* i = concat;
+//     // printf("concatenation: %s\n", concat);
+//     return opendir(concat, 0);
 
-int recurse_dir(const char* dir_name, const char* prefix, int recursion_level) {
-    // printf("look this is the length (%s) \n", char_arr_size(dir_name));
-    // const char* test = concat_char_arr("hello", "world");
-    // printf("testing\n");
-    // printf("aughhhhhhh %s", test);
-    // printf("prefix length (%d) ", strlen(prefix));
-    // if (prefix) {
-    //     printf("helloooo");
-    // } else{
-    //     printf("hone");
-    // }
-    // printf("%d", sizeof(dir_name));
-    // if (prefix) {dir_name = prefix+dir_name;}
+// }
 
-    if (prefix) {
-        dir_name = concat_char_arr(prefix, dir_name);
-        // printf("concatenation importante: %s\n", dir_name);
-    }
 
-    printf("%s \n", dir_name);
+
+// int recurse_dir(HDIR dir) {
+
     
-    HDIR dir = opendir(dir_name, 0);
-  
-    if (is_error(dir)){
-        printf("this right here is an error I'm throwing cuz you tried to open %s ", dir);
-        return 1;
-    }
-    printf("directory opened");
-    struct dirent de;
 
-    while (readdir(dir, &de)) {
-        printf("%s (%u bytes) \n", de.name, de.size);
-        // int test = recurse_dir(de.name, dir_name, 1);
-    }
+    
+//     struct dirent de;
+//     bool is_dir = false;
+//     while (readdir(dir, &de)) {
+//         // printf("%s (%u bytes) \n", de.name, de.size);
+        
+//         HDIR dir2 = get_concat_dir(dir_name, de.name);
+//         if (!is_error(dir2)){
+//             printf("is a directory");
+//             is_dir = true;
+//         } else {
+//             printf("not a directory");
+//         }
+
+//         if (is_dir) {
+//             recurse(dir2);
+//         }
+//         // struct dirent de2;
+//         // // printf("the new name is %s\n", new_name);
+//         // while (readdir(dir2, &de2)) {
+        
+//         //     printf("=-=-= %s (%u bytes) \n", de2.name, de2.size);
+//         // }
+//         is_dir = false;
+//     }
+
+//     return 0;
+// }
+
+// void print_dir(int recursion_level) {
+//     printf("pass");
+// }
+
+// const char* concatenate(const char* lhs, const char* rhs) {
+//     // concatenates two strings
+//     // takes lhs, rhs, returns lhs/rhs
+    
+//     int lhs_size = strlen(lhs);
+//     int rhs_size = strlen(rhs);
+
+//     // printf("this word %s has size %d \n", lhs, lhs_size);
+//     // printf("this word %s has size %d \n", rhs, rhs_size);
+
+//     int new_size = lhs_size + rhs_size;
+
+//     char *buffer[new_size+1];
 
 
-    closedir(dir);
+//     for (unsigned int i=0; i < lhs_size; i++) {
+//         // printf("%d\n", i);
+//         buffer[i] = lhs[i];
+//     }
+//     concat[lhs_size] = "/";
 
-    return 0;
+//     printf("\n");
+
+//     for (unsigned int i=0; i < rhs_size; i++) { 
+//         // printf("%d\n", lhs_size+i);
+//         buffer[lhs_size+i+1] = rhs[i];
+//     }
+
+
+    
+
+//     return buffer;
+// } 
+
+const char *cat(const char *lhs, const char *rhs)
+{
+
+	const char *a = lhs;
+	const char *b = rhs;
+
+	int new_size = strlen(a) + strlen(b);
+
+    printf("strlen a %d \n", strlen(a));
+
+	char buffer[new_size];
+
+	int i = 0;
+	while (*a && *a != ' ') {
+        printf("%d \n", i);
+		buffer[i++] = *a++;
+	}
+	buffer[i] = 0;
+
+	if (*a) a++;
+
+
+	printf("current state %s \n", buffer);
+
+
+	while (*b && *b != ' ') {
+		buffer[i++] = *b++;
+	}
+	buffer[i] = 0;
+
+	if (*b) b++;
+
+	printf("final state %s \n", buffer);
+
+    const char* ret = buffer;
+    return ret;
+
 }
-
-void print_dir(int recursion_level) {
-    printf("pass");
-}
-
 
 int main(const char *cmdline)
 {
@@ -115,23 +180,17 @@ int main(const char *cmdline)
 
     // recurse_dir(input, NULL, 0);
 
+    // HDIR dir = opendir(dir_name, 0);
 
-    HDIR dir = opendir(input, 0);
-  
+    // if (is_error(dir)) {
+    //     printf("big time uh-oh");
+    //     return 1;
+    // }
+
+    const char *test = cat("hello", "world");
+    printf("PLEASE GOD %s \n", test);
+    // recurse_dir(dir);
     
-    struct dirent de;
-
-    while (readdir(dir, &de)) {
-        // printf("%s (%u bytes) \n", de.name, de.size);
-        const char* new_name = concat_char_arr(input, de.name);
-        HDIR dir2 = opendir(new_name, 0);
-        struct dirent de2;
-        printf("the new name is %s\n", new_name);
-        while (readdir(dir2, &de2)) {
-        
-            printf("=-=-= %s (%u bytes) \n", de2.name, de2.size);
-        }
-    }
 
 
 
